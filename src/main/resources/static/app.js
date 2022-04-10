09687a8a182f6ddafe75221ebbc5f53a8d617998
Alexander Lazarchuk <olazarchuk@markettraders.com>
2022-04-10 04:32:52 +0300

    #dev2

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -11,27 +11,13 @@ function setConnected(connected) {
     $("#greetings").html("");
 }
 
-// function connect() {
-//     $.ajax({
-//         url: '/authentication/token',
-//         headers: {"Authorization": "SuperSecureToken"},
-//         success: function (result) {
-//             webSocketConnect(result)
-//         }
-//     })
-// }
-
 function webSocketConnect(authToken) {
     console.log("Auth Token: " + authToken);
     var socket = new SockJS('/websocket/connect?authentication=' + authToken);
     stompClient = Stomp.over(socket);
-//    stompClient.connect({}, function (frame) {
     stompClient.connect({}, function () {
         setConnected(true);
-//        console.log('Connected: ' + frame);
-//        stompClient.subscribe('/topic/greetings', function (greeting) {
         stompClient.subscribe('/topic/greetings', function () {
-//            showGreeting(JSON.parse(greeting.body).content);
         });
     });
 }
@@ -48,16 +34,11 @@ function sendName() {
     stompClient.send("/app/hello", {}, JSON.stringify({'name': $("#name").val()}));
 }
 
-// function showGreeting(message) {
-//     $("#greetings").append("<tr><td>" + message + "</td></tr>");
-// }
-
 $(function () {
     $("form").on('submit', function (e) {
         e.preventDefault();
     });
     $("#connect").click(function () {
-//        connect();
         webSocketConnect("1dfa537f-d46f-451e-9daa-ce26a58c6583");
     });
     $("#disconnect").click(function () {
